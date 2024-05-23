b738c67c44226ec565be20eef85d1fdd0b1da262
narges-malek <106173610+narges-malek@users.noreply.github.com>
2024-05-22 23:27:05 -0400

    Update rmse.py

--- rmse.py
+++ rmse.py
@@ -1,7 +1,13 @@
 import numpy as np
 
 def rmse(predictions, targets):
+    r"""
+    RMSE (Root Mean Square Error) = 
+    $\sqrt{\frac{1}{n}\sum_{i=1}^{n}(y_i - \hat{y_i})^2}$,
+    where $y_i$ are the true values, $\hat{y_i}$ are the predicted values,
+    and $n$ is the total number of samples.
+    """
     pred = np.array(predictions)
     tar = np.array(targets)
-    rmse = None # TODO: Implement RMSE Calculation here...
+    rmse = np.sqrt(np.mean((tar-pred)**2))
     return rmse
