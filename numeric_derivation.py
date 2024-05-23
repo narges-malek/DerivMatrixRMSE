c05ab4062f647c5942cb98b8fafb8d00835e3f7a
narges-malek <106173610+narges-malek@users.noreply.github.com>
2024-05-22 23:19:50 -0400

    Update numeric_derivation.py

--- numeric_derivation.py
+++ numeric_derivation.py
@@ -1,3 +1,13 @@
-
 def derive(f, x, h=0.0001):
-    return None  # TODO: implement this function 
+    r"""
+    Calculate the derivative of function f at point x using the central difference method.
+
+    Parameters:
+    f (function): The function for which the derivative is to be calculated.
+    x (float): The point at which the derivative is calculated.
+    h (float): A small value for computing the difference. Default is 0.0001.
+
+    Returns:
+    float: The derivative of function f at point x.
+    """
+    return (f(x + h) - f(x - h)) / (2 * h) 
